57eef6bc89853362dd69d25b19ea6f55761dc724
FreeYoshino <abc0940415@gmail.com>
2024-12-02 20:42:07 +0800

    Add standard variable in Item Class

--- Assets/Inventory/InventoryScripts/Item.cs
+++ Assets/Inventory/InventoryScripts/Item.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Item", menuName = "Inventory/New Item")]
 public class Item : ScriptableObject
 {
-        
+    // 繼承ScriptableObject類 儲存Item的資訊
+    public string itemName;
+    public Sprite itemImage;
+    public int itemHeld;
+    [TextArea]
+    public string itemInfo;
+
 }
