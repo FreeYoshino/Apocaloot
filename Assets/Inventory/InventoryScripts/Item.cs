5a36a182397f505e5857c36305d6a912e33414d8
FreeYoshino <abc0940415@gmail.com>
2024-12-02 20:35:25 +0800

    Create Item.cs use ScriptableObject

---
+++ Assets/Inventory/InventoryScripts/Item.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/New Item")]
+public class Item : ScriptableObject
+{
+        
+}
