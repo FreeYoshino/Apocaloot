5fd5ad7bb887c86e8f7eea7116b154c5709f7a3b
FreeYoshino <abc0940415@gmail.com>
2024-12-09 20:32:53 +0800

    設定角色選取物

---
+++ Assets/Player/Script/CharacterSelect.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelect : MonoBehaviour
+{
+    public CharacterData characterData;
+    public void CharacterOnSelected()
+    {
+        CharacterManager.InitializeCharacterData(characterData);
+    }
+}
