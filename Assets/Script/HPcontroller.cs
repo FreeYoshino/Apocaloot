6bc16df6cf3fef3990f9cff5f2664a86f9e1f0eb
glodlion <magic09535@gmail.com>
2024-12-11 00:49:01 +0800

    BOss的跳躍

--- Assets/Script/HPcontroller.cs
+++ Assets/Script/HPcontroller.cs
@@ -1,24 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HPcontroller : MonoBehaviour
 {
     GameObject HP;
+    GameObject Boss;
     float time=0;
     // Start is called before the first frame update
     void Start()
     {
         HP = GameObject.Find("¦å±ø");
+        Boss = GameObject.Find("BOSS");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (time > 5)
+        if (time > 3)
         {
             DecreaseHP();
+            BossHurt();
             time -= 5;
         }
         time += Time.deltaTime;
@@ -28,5 +32,9 @@ public class HPcontroller : MonoBehaviour
     void DecreaseHP()
     {
         HP.GetComponent<Image>().fillAmount -=0.2f;
+    }
+    void BossHurt()
+    {
+        Boss.GetComponent<Animator>().SetTrigger("BossHurt");
     }
 }
