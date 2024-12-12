45d4f9d6671ee04ed6d2d1c9b62c0bc6d4e9c030
FreeYoshino <abc0940415@gmail.com>
2024-12-12 19:28:56 +0800

    添加BootScene 加載各Manager

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -66,7 +66,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public static void LoadSceneFirst()
+    public static void LoadFirstScene()
     {
         SceneManager.LoadScene("first");
     }
