c650de5a0615c08d71cecaca9855f686c2d47c18
FreeYoshino <abc0940415@gmail.com>
2024-12-13 20:42:34 +0800

    完成槌子攻擊

--- Assets/Player/Script/PlayerAttack.cs
+++ Assets/Player/Script/PlayerAttack.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    CharacterData characterData;
+    CharacterData characterData;            // 角色Data
+    private float timer = 0f;               // 攻擊時間的timer
+    private bool isAttacking = false;       // 是否在攻擊
     private void Start()
     {
         characterData = CharacterManager.GetCharacterData();
@@ -16,6 +18,17 @@ public class PlayerAttack : MonoBehaviour
         {
             Attack();
         }
+        if (isAttacking)
+        {
+            timer += Time.deltaTime;
+            if (timer >= characterData.characterTimeToAttack)
+            {
+                timer = 0f;
+                isAttacking = false;
+                if (characterData.characterType == CharacterData.CharacterType.Hammer) CharacterManager.GetCharacterObject().transform.Find("AttackArea").gameObject.SetActive(isAttacking);  
+            }
+        }
+
     }
     private void Attack()
     {
@@ -28,9 +41,17 @@ public class PlayerAttack : MonoBehaviour
                 Debug.Log("遠程攻擊");
                 break;
             case CharacterData.CharacterType.Hammer:
-                Debug.Log("近戰攻擊");
+                HammerAttack();
                 break;
         }
-        
+    }
+
+    private void HammerAttack()
+    {
+        // 槌子攻擊
+        Debug.Log("槌子攻擊");
+        GameObject attackArea = CharacterManager.GetCharacterObject().transform.Find("AttackArea").gameObject;
+        isAttacking = true;
+        attackArea.SetActive(isAttacking);
     }
 }
