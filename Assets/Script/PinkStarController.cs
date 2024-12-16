c56fe968b67bfee983675bc91e49dd90dd664957
glodlion <magic09535@gmail.com>
2024-12-16 21:13:00 +0800

    派小星小怪攻擊

--- Assets/Script/PinkStarController.cs
+++ Assets/Script/PinkStarController.cs
@@ -8,10 +8,12 @@ public class PinkStarController : MonoBehaviour
 {
     float key = 1f;          //BOSS¬O§_Âà¦V
     float dis = 4f;
-    float AttackSpeed = 0.3f;
+    float AttackSpeed = 3f;
+    float AttackAnimationSpeed = 0.3f;
     float AttackCD = 2f;
     float time = 0f;
     Vector2 start = new Vector2();
+    Vector2 now = new Vector2();
     Rigidbody2D rigidbody2D;
     Animator animator;
 
@@ -26,20 +28,35 @@ public class PinkStarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (start.x > start.x + dis)
-        {
+        time += Time.deltaTime;
+        now = transform.position;
+        if (now.x > start.x + dis)
+        {  
             key = -1f;
+            //animator.SetTrigger("PinkStarAttack2Idle");
         }
-        else if (start.x < start.x - dis)
+        else if (now.x < start.x - dis)
         {
             key = 1f;
+            //animator.SetTrigger("PinkStarAttack2Idle");
         }
+        Debug.Log(time);
         if (time > AttackCD)
         {
-            animator.SetTrigger("PinkStarAttack");
+            if (key < 0f)
+            {
+                animator.SetTrigger("PinkStarAttack");
+                
+            }
+            else
+            {
+                animator.SetTrigger("PinkStarAttackRight");
+            }
+            animator.speed = AttackAnimationSpeed;
+            transform.localScale = new Vector2(5f * key, 5f);
+            rigidbody2D.velocity = new Vector2(AttackSpeed * key, rigidbody2D.velocity.y);
+            time = 0; 
         }
-        transform.localScale = new Vector2(5f * key, 5f);
-        rigidbody2D.velocity = new Vector2(AttackSpeed * key, rigidbody2D.velocity.y);
     }
 
 }
