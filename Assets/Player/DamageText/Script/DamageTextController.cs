ccf0f992af919a00b186e86aa212ac0da542b217
FreeYoshino <abc0940415@gmail.com>
2024-12-16 21:17:04 +0800

    設定DamageTextController

---
+++ Assets/Player/DamageText/Script/DamageTextController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class DamageTextController : MonoBehaviour
+{
+    public float floatSpeed = 1.5f;     // 漂浮速度
+    public float lifetime = 1.0f;       // 文字存在時間
+    public float fadeSpeed = 2.0f;      // 淡出的速度
+    private Color color;                // 顏色
+    private TMP_Text text;              // TMP
+    private void Awake()
+    {
+        text = GetComponent<TMP_Text>();
+        color = text.color;
+    }
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.up * floatSpeed * Time.deltaTime);      // 文字漂浮向上
+        color.a -= fadeSpeed * Time.deltaTime;                              // 文字淡出
+        text.color = color;
+        if (color.a <= 0)
+        {
+            // 文字完全透明
+            Destroy(gameObject);
+        }
+    }
+    public void SetDamageText(int damage)
+    {
+        text.text = damage.ToString();
+    }
+}
