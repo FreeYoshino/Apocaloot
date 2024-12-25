df662c8e1611f99193fb75a205516a292672ef51
glodlion <magic09535@gmail.com>
2024-12-25 22:29:46 +0800

    小細節

--- Assets/Script/Canno2BallController.cs
+++ Assets/Script/Canno2BallController.cs
@@ -6,37 +6,33 @@ using UnityEngine;
 public class CannoBall2Controller : MonoBehaviour
 {
     // Start is called before the first frame update
-    GameObject canno, Player;
     GameObject right_border, left_border;
     float flyForce = 100f;
     float MaxSpeed = 6f;
-    float direction;
     float time;
     float speed = 150f;
+    float bulletSpeed = 5f; // 子彈速度
     Rigidbody2D rb;
     Animator animator;
+    GameObject player;
+    Vector2 direction;
     void Start()
     {
+        player = GameObject.FindWithTag("Player");
         animator = GetComponent<Animator>();
-        canno = GameObject.Find("Canno2");
         right_border = GameObject.Find("right_border");
         left_border = GameObject.Find("left_border");
-        //Player = GameObject.Find("Player");
-        direction = canno.transform.localScale.x;
-        
-        gameObject.transform.localScale = new Vector2(direction, 5f);
         rb = GetComponent<Rigidbody2D>();
-        if (direction > 0)
-        {
-            rb.velocity = new Vector2(-MaxSpeed, 0);
-        }
-        else
-            rb.velocity = new Vector2(MaxSpeed, 0);
-    }
+        direction = (player.transform.position - transform.position).normalized;
+        
 
+    }
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("方向: " + direction);
+        rb.velocity = direction * bulletSpeed;
+        Debug.Log("速度: " + rb.velocity);
         if (gameObject.transform.position.x > right_border.transform.position.x)
         {
             //animator.SetTrigger("CannoBall");
@@ -57,9 +53,9 @@ public class CannoBall2Controller : MonoBehaviour
         {
             collision.gameObject.GetComponent<PlayerHurt>().Hurt(20f);
             Debug.Log("攻擊敵人");
-            Destroy(gameObject);
+            
         }
-        
+        Destroy(gameObject);
     }
     
 }
